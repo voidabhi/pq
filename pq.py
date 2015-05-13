091533484ae599414c0652a27f5b4e32a13eb2f1
Abhijeet Mohan <void.aby@gmail.com>
2015-05-13 23:49:35 +0530

    added push script

--- pq.py
+++ pq.py
@@ -3,3 +3,20 @@
 __author__='Abhijeet Mohan'
 
 from gcm import GCM
+
+gcm = GCM(API_KEY)
+data = {'param1': 'value1', 'param2': 'value2'}
+
+# Plaintext request
+reg_id = '12'
+gcm.plaintext_request(registration_id=reg_id, data=data)
+
+# JSON request
+reg_ids = ['12', '34', '69']
+response = gcm.json_request(registration_ids=reg_ids, data=data)
+
+# Extra arguments
+res = gcm.json_request(
+    registration_ids=reg_ids, data=data,
+    collapse_key='uptoyou', delay_while_idle=True, time_to_live=3600
+)
