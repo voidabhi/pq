e3686732e3d2c3972e6cb1561da19bb910acb8d8
Abhijeet Mohan <void.aby@gmail.com>
2015-07-09 23:36:57 +0530

    Update pq.py

--- pq.py
+++ pq.py
@@ -17,6 +17,7 @@ data = {'param1': 'value1', 'param2': 'value2'}
 # downloading document from url
 r = requests.get(DOC_URL)
 data = r.text
+row = data.split('\n')
 
 # Plaintext request
 reg_id = '12'
