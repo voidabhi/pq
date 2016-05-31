ce322e2f48c5491c1840332fce01b19e3ee2a809
Abhijeet Mohan <void.aby@gmail.com>
2016-05-31 23:57:05 +0530

    Update pq.py

--- pq.py
+++ pq.py
@@ -8,34 +8,39 @@ import ConfigParser
 from pprint import pprint
 
 # Read config
-config = ConfigParser.RawConfigParser()
-config.read('constants.cfg')
+def read_config():
+    config = ConfigParser.RawConfigParser()
+    config.read('constants.cfg')
+    return (config.get('doc_url'), config.get('API_KEY'), config.get('REG_ID'))
+    
+# Read from excel sheet url
+def read_from_url(url):
+    r = requests.get(url)
+    data = r.text
+    rows = data.split('\n')
+    return rows
+    
+def send_pn():
+    (DOC_URL, API_KEY, REG_ID) = read_config()
+    
+    # bulding GCM object from API_KEY
+    gcm = GCM(API_KEY)
 
-# Constant doc url
-DOC_URL = config.get('doc')
+    # downloading document from url
+    rows = read_from_url(DOC_URL)
+    
+    # Plaintext request
+    for row in rows:
+        # making gcm request
+        gcm.plaintext_request(registration_id=REG_ID, data=row)
 
-# updating api key
-API_KEY = config.get('API_KEY')
+    # Json request
+    return gcm.json_request(
+        registration_ids=reg_ids, data=data,
+        collapse_key='uptoyou', delay_while_idle=True, time_to_live=3600
+    )
 
-# bulding GCM object from API_KEY
-gcm = GCM(API_KEY)
-
-# downloading document from url
-r = requests.get(DOC_URL)
-data = r.text
-rows = data.split('\n')
-
-# Plaintext request
-reg_id = config.get('REG_ID')
-for row in rows:
-    # making gcm request
-    gcm.plaintext_request(registration_id=reg_id, data=row)
-
-# Extra arguments
-res = gcm.json_request(
-    registration_ids=reg_ids, data=data,
-    collapse_key='uptoyou', delay_while_idle=True, time_to_live=3600
-)
-
-# output response
-pprint(res)
+if __name__ == '__main__':
+    res = send_pn()
+    # output response
+    pprint(res)
