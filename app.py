113a68e5250549e018a551873eda4c5c85bdf171
Abhijeet Mohan <void.aby@gmail.com>
2016-06-23 23:47:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
 
 
-from flask import app
+from flask import Flask, jsonify, make_response, request
+from pq import send_pn
+
+app = Flask(__name__)
+
+@app.route('/')
+@crossdomain(origin='*')
+def index():
+    '''
+    Endpoint for triggering push notification
+    '''
+    return make_response(jsonify({ 'success': True }), 200)
 
 if __name__ == '__main__':
     app.run(debug=True)
