a4a8dcce07fefa1dfa1461418987e2b01771c795
Abhijeet Mohan <void.aby@gmail.com>
2016-10-01 23:59:26 +0530

    Update benchmark.sh

--- benchmark.sh
+++ benchmark.sh
@@ -3,3 +3,6 @@
 
 # check if apache benchmark exists
 command -v ab >/dev/null 2>&1 || { echo >&2 "I require foo but it's not installed.  Aborting."; exit 1; }
+
+# run benchmark
+ab -p post_loc.txt -T application/json -c 10 -n 2000 http://localhost:8080/send
