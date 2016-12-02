d565862b803404a7113d92fb7b17f337607c7789
Abhijeet Mohan <void.aby@gmail.com>
2016-12-02 23:46:08 +0530

    Add support for sending ios notification

--- pq.py
+++ pq.py
@@ -48,6 +48,27 @@ def send_pn():
         collapse_key='uptoyou', delay_while_idle=True, time_to_live=3600
     )
 
+def send_pn_ios():
+def send_push_message(token, payload):
+    # the certificate file generated from Provisioning Portal
+    certfile = 'my_app_apns_certificate.pem'
+ 
+    # APNS server address (use 'gateway.push.apple.com' for production server)
+    apns_address = ('gateway.sandbox.push.apple.com', 2195)
+ 
+    # create socket and connect to APNS server using SSL
+    s = socket.socket()
+    sock = ssl.wrap_socket(s, ssl_version=ssl.PROTOCOL_SSLv3, certfile=certfile)
+    sock.connect(apns_address)
+ 
+    # generate APNS notification packet
+    token = binascii.unhexlify(token)
+    fmt = "!cH32sH{0:d}s".format(len(payload))
+    cmd = '\x00'
+    msg = struct.pack(fmt, cmd, len(token), token, len(payload), payload)
+    sock.write(msg)
+
+
 if __name__ == '__main__':
     res = send_pn()
     # output response
